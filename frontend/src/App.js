ac1a5314c50b56b349dc50d2159bf2476fbd31ba
Erica Bäckman <112472987+EricaBackman@users.noreply.github.com>
2023-05-26 11:43:51 +0200

    Setup ChatRoom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ProfilePage from './pages/ProfilePage';
+import ChatRoom from './pages/ChatRoom'
 
 function App() {
   return (
     <div className="App">
         <ProfilePage/>
+        <ChatRoom />
     </div>
   );
 }
