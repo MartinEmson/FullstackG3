df2066edabf1c245f65f9b0c8f0324273373776b
Erica Bäckman <112472987+EricaBackman@users.noreply.github.com>
2023-06-07 21:23:04 +0200

    Replay

--- frontend/src/pages/ChatRoom.js
+++ frontend/src/pages/ChatRoom.js
@@ -2,30 +2,27 @@ import React, { useEffect, useState, useContext, useRef } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
 import { AuthContext } from '../context/AuthContext'
-// import { useNavigate } from 'react-router-dom'
-
-
 
 const ChatRoom = () => {
-  // const navigate = useNavigate()
-  const { loggedInUserId } = useContext(AuthContext);
+  const chatBottomRef = useRef(null)
+  const { loggedInUserId } = useContext(AuthContext)
 
   const [validToken, setValidToken] = useState(false)
   const [messages, setMessages] = useState([])
   const [error, setError] = useState(false)
-  // const [answerRecipientId, setAnswerRecipientId] = useState(null)
-  const [answerRecipientMessage, setAnswerRecipientMessage] = useState('')
+  // const [answerRecipientMessage, setAnswerRecipientMessage] = useState('')
   const [answer, setAnswer] = useState(false)
   const [senders, setSenders] = useState([])
   const [answerName, setAnswerName] = useState('')
-
-  const chatBottomRef = useRef(null);
+  const [replyingToMessage, setReplyingToMessage] = useState(null);
+// const [replyingToSender, setReplyingToSender] = useState(null);
 
   useEffect(() => {
     if (chatBottomRef.current) {
-      chatBottomRef.current.scrollIntoView({ behavior: 'smooth' });
+      chatBottomRef.current.scrollIntoView({ behavior: 'smooth' })
     }
-  }, [messages]); // Trigger the effect whenever the messages state changes
+  }, [messages]) // Trigger the effect whenever the messages state changes
+
   // Kolla så att ett giltigt token finns
   useEffect(() => {
     const checkToken = async () => {
@@ -44,7 +41,6 @@ const ChatRoom = () => {
       }
     }
     checkToken()
-    console.log(validToken)
   }, [validToken])
 
   useEffect(() => {
@@ -70,7 +66,7 @@ const ChatRoom = () => {
       }
     }
     fetchMessages()
-  }, [])
+  }, [loggedInUserId])
 
   const [newMessage, setNewMessage] = useState({
     sender_id: loggedInUserId,
@@ -92,7 +88,7 @@ const ChatRoom = () => {
     const messageData = {
       sender_id: loggedInUserId,
       recipient_id: newMessage.recipient_id,
-      message: newMessage.message
+      message: `${newMessage.message} (Replying to: ${replyingToMessage})`,
     }
 
     axios
@@ -104,11 +100,15 @@ const ChatRoom = () => {
           ...prevMessages,
           {
             ...newMessage,
+            message: `${newMessage.message} (Replying to: ${replyingToMessage})`,
             message_id
           }
         ])
+        setAnswer(false)
         event.target.reset()
-        window.location.reload()
+        setReplyingToMessage(null);
+        // setReplyingToSender(null);
+
         console.log(messages)
         console.log(response.data)
       })
@@ -131,6 +131,7 @@ const ChatRoom = () => {
     }
   }
 
+  //Handle Replay-button.
   const handleAnswer = async (
     event,
     recipientId,
@@ -140,13 +141,17 @@ const ChatRoom = () => {
     event.preventDefault()
     setAnswer(true)
     setAnswerName(answerName)
-    setAnswerRecipientMessage(recipientMessage)
+    // setAnswerRecipientMessage(recipientMessage)
     setNewMessage((prevMessage) => ({
       ...prevMessage,
       recipient_id: recipientId
     }))
+    setReplyingToMessage(recipientMessage);
+    // setReplyingToSender(answerName);
     console.log(recipientId)
   }
+
+  // const loggedIn = localStorage.getItem('loggedInUserId')
 
   return (
     <>
@@ -160,60 +165,87 @@ const ChatRoom = () => {
                   const sender = senders.find(
                     (user) => user.user_id === message.sender_id
                   )
-                  // console.log(sender.user_firstname)
+                  const isUserMessage = message.sender_id === loggedInUserId
+                  const isDeleteButtonVisible = isUserMessage
+                  // const isReplyToLoggedInUser = message.recipient_id === loggedInUserId;
+                  // const isReplyingToThisMessage = replyingToMessage === message.message && replyingToSender === sender.user_firstname;
+
                   return (
-                    <div key={message.message_id} className="message">
-                      <p className="theMessage">
-                        {sender.user_firstname}
-                        {message.message}
-                        {message.sender_id}
-                        {message.recipient_id}
-                        {message.message_id}
-                      </p>
-                      <button
-                        type="button"
-                        onClick={(event) =>
-                          handleDelete(event, message.message_id)
-                        }
-                      >
-                        Ta bort
-                      </button>
-                      <button
-                        type="button"
-                        onClick={(event) =>
-                          handleAnswer(
-                            event,
-                            message.recipient_id,
-                            sender.user_firstname,
-                            message.message
-                          )
-                        }
-                      >
-                        Svara
-                      </button>
-                    </div>
+                    <MessageContainer
+                      key={message.message_id}
+                      className="messageWrapper"
+                      isUserMessage={isUserMessage}
+                    >
+                      <div className="senderInfo">
+                        <img
+                          src={sender.image}
+                          alt="Profile"
+                          className="profileImage"
+                        />
+                        <p className="senderName">{sender.user_firstname}</p>
+                      </div>
+                      <div className="theMessageWrapper">
+                      {/* {isReplyingToThisMessage && (
+          <p className="replyingToMessage">
+            Replying to: {replyingToSender} - {replyingToMessage}
+          </p>
+        )} */}
+                        <p className="theMessage">{message.message}</p>
+                      </div>
+                      <div className="buttonWrapper">
+                        {isDeleteButtonVisible && (
+                          <button
+                            type="button"
+                            onClick={(event) =>
+                              handleDelete(event, message.message_id)
+                            }
+                          >
+                            Delete
+                          </button>
+                        )}
+                        <button
+                          type="button"
+                          onClick={(event) =>
+                            handleAnswer(
+                              event,
+                              message.recipient_id,
+                              sender.user_firstname,
+                              message.message
+                            )
+                          }
+                        >
+                          Reply
+                        </button>
+                      </div>
+                    </MessageContainer>
                   )
                 })}
               </div>
-              <ChatInput>
-                <form method="post" onSubmit={handleSubmit}>
-                  {answer && `Svara ${answerName} ${answerRecipientMessage}`}
+              <div className="formWrapper">
+                <form method="post" onSubmit={handleSubmit} className="form">
+                  {answer && (
+                    <div className="ifAnswer">
+                      <p>Answer {answerName}</p>
+                    </div>
+                  )}
                   <input
                     type="text"
                     name="message"
                     onChange={handleChange}
-                    placeholder="Skriv ditt meddalande här"
+                    placeholder="Write someting.."
                   />
-                  <button type="submit">Skicka</button>
+                  <button type="submit" className="submitButton">
+                    Send
+                  </button>
                   <div ref={chatBottomRef} />
-                  {error && <p>Något blev fel, försök igen.</p>}
+                  {error && <p>Something went wrong, please try again.</p>}
                 </form>
-              </ChatInput>
+              </div>
             </RightSide>
           </ChatWindow>
         </ChatBg>
       ) : (
-        <h1>Vänligen logga in</h1>
+        <h1>Please log in first</h1>
       )}
     </>
   )
@@ -226,6 +258,7 @@ const ChatBg = styled.div`
   justify-content: center;
   height: 80vh;
   width: 100%;
+  font-family: Inter, sans-serif;
 `
 const ChatWindow = styled.div`
   background-color: white;
@@ -236,28 +269,96 @@ const ChatWindow = styled.div`
   border-radius: 15px;
   overflow-y: scroll;
 `
+
 const LeftSide = styled.div``
 const RightSide = styled.div`
-input[type=text] {
-  width: 15vw;
-  transition: width 0.4s ease-in-out;
-}
+  .formWrapper {
+    display: flex;
+    height: 10vh;
+    border-top: solid #d9d9d9 0.5px;
+    padding: 0.2vh 0 0 0;
+  }
 
-input[type=text]:focus {
-  width: 80vw;
-}
+  input {
+    border: none;
+    width: 60vw;
+  }
 
-button {
-  /* position: absolute;
-  right: 0; */
-}
+  input[type='text'] {
+    width: 60vw;
+    padding: 4vh 0 0 4vw;
+  }
+
+  input[type='text']:focus {
+    outline-color: transparent;
+    max-width: 60vw;
+  }
+
+  .submitButton {
+    border: none;
+    background-color: transparent;
+    margin: 0 0 0 6vw;
+    cursor: pointer;
+  }
+
+  .ifAnswer {
+    font-size: 12px;
+    color: grey;
+    margin: 0;
+    padding: 0;
+  }
+
+  .ifAnswer p {
+    margin: 0.5vh 0 0 1vw;
+  }
+
+  .senderInfo {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .profileImage {
+    height: 40px;
+    width: 40px;
+    margin: 0 0 0 1vw;
+    padding: 0 0 1vh 0;
+  }
+
+  .senderName {
+    padding: 1vh 0 0 1vw;
+    margin: 0;
+  }
+
+  .theMessage {
+    margin: 0;
+    word-wrap: break-word;
+    max-width: 60vw;
+  }
+
+  .theMessageWrapper {
+    background: #d9d9d9;
+    border-radius: 30px;
+    padding: 1.5vh 4vw 1.5vh 4vw;
+    margin: 0 0 0 3vw;
+    max-width: 60vw;
+    /* align-self: ${({ isUserMessage }) =>
+      isUserMessage ? 'flex-end' : 'flex-start'}; */
+  }
 `
-const ChatInput = styled.div`
-display: flex;
-height: 7vh;
-/* position: sticky;
-bottom: 0; */
-border: solid black 1px
+const MessageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 3vh 0;
 
+  .buttonWrapper {
+    margin-left: 3vw;
+  }
 
+  button {
+    background-color: transparent;
+    border: none;
+    margin: 0;
+    padding: 0 1vw;
+    cursor: pointer;
+  }
 `
